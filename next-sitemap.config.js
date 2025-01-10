dbfab24376ef3386d0a9757dce6f3e3efc629359
Sajid Ali <livepayout@gmail.com>
2025-01-10 11:18:05 +0500

    updated sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://www.livepayout.org', // Replace with your actual domain
+    generateRobotsTxt: true, // (Optional) Generate robots.txt file
+    changefreq: 'daily', // Frequency of changes
+    priority: 0.7, // Default priority for pages
+    sitemapSize: 5000, // Number of URLs per sitemap file
+  };
+  
